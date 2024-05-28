e0aaaed91250f0a0518fcff4380543540f4fe887
MoriFGC <abdulmoha@outlook.it>
2024-05-28 17:53:53 +0200

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
-import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Welcome from './components/Welcome'
 import fantasy from "/Books/fantasy.json";
 import AllTheBooks from './components/AllTheBooks'
-import NavBar2 from './components/NavBar2';
+import NavBar2 from './components/NavBar2'
 
 
 function App() {
   return (
     <>
       <NavBar2 />
-      {/* <NavBar /> */}
       <Welcome />
       <AllTheBooks books = {fantasy} />
       <Footer />
