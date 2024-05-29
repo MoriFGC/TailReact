80e26ef15ece5d51b75a8c4b0e74b93fca86e677
MoriFGC <abdulmoha@outlook.it>
2024-05-29 19:58:43 +0200

    gg

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -9,32 +9,20 @@ import fantasy from "/Books/fantasy.json";
 
 const [testo, setTesto] = useState('');
 
-    
-
-     function handleChange(value) {
-      setTesto(value)
-      
-      const books = fantasy.filter((book) => {
-        return (
-          book.title.toLowerCase().includes(value)
-        )
-      });
-      console.log(books);
-     };
-
   return (
     <>
       <div className='bg-white mx-auto w-[20%] rounded-[10px] h-[2.5rem] py-[0px] px-[15px] flex items-center '>
           <input className='bg-transparent border-none h-[100%] text-[1.25rem] w-[100%] ms-[5px] focus:outline-none'
-          type='text' 
+          type='search' 
           placeholder='Search Books...' 
           value={testo} 
-          onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) => setTesto(e.target.value)}
           />
           <MagnifyingGlassCircleIcon className='w-[40px] text-pink-400'/>
         </div>
       <main className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3  mt-5'>
-         {props.books.map((book) => ( <SingleBook book = {book}/>))}
+         {props.books.filter(book => book.title.toLowerCase().includes(testo))
+         .map((book) => ( <SingleBook book = {book}/>))}
       </main>
     </>
     
